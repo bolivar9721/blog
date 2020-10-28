f073f997fadbae1ccfe79797a801a39387a64c62
bolivar9721 <raul.ruiz.bolivar@gmail.com>
2020-10-28 19:26:54 +0100

    Cambiando el nav

--- vista/v_nav.php
+++ vista/v_nav.php
@@ -1,9 +1,9 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg p-3 mb-2 sticky-top d-flex">
-    <a class="flex-grow-1" href="index.php?lugar=3">
-    <img class="rounded-circle" src="vista/img/logos/<?php 
-          echo $logotipo[0]['logotipo'];
-        ?>"/>
-    </a>
+    <span class="flex-grow-1">
+      <a href="index.php?lugar=3">
+        <img class="rounded-circle" src="vista/img/logos/<?php echo $logotipo[0]['logotipo'];?>"/>
+      </a>
+    </span>
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link<?php if($lugar == 0){echo " active";} ?>" href="index.php?lugar=0">Artículos</a> 
