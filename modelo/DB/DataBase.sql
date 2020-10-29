46a49633f8c9313bf03e5bb1d01ef0ecfcaaceab
bolivar9721 <raul.ruiz.bolivar@gmail.com>
2020-10-29 21:41:36 +0100

    Cambiando imagenes

--- modelo/DB/DataBase.sql
+++ modelo/DB/DataBase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 25-10-2020 a las 23:44:28
+-- Tiempo de generación: 29-10-2020 a las 21:40:31
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.34
 
@@ -31,20 +31,25 @@ USE `blog`;
 
 CREATE TABLE `articulo` (
   `id_articulo` int(11) NOT NULL,
-  `imagenes_id` int(11) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
   `titulo` varchar(255) NOT NULL,
   `articulo` varchar(8000) NOT NULL,
   `fecha` varchar(8) NOT NULL,
-  `etiqueta_id` int(11) NOT NULL
+  `etiqueta_id` int(11) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `articulo`
 --
 
-INSERT INTO `articulo` (`id_articulo`, `imagenes_id`, `titulo`, `articulo`, `fecha`, `etiqueta_id`) VALUES
-(1, 1, 'titulo de programa', 'articulo de programa', 'programa', 1),
-(2, 2, 'titulo de desarrollo', 'articulo de desarrollo', 'develop', 2);
+INSERT INTO `articulo` (`id_articulo`, `imagen`, `titulo`, `articulo`, `fecha`, `etiqueta_id`, `descripcion`) VALUES
+(1, 'imagen1.jpg', 'titulo de programa', 'articulo de programa', 'programa', 1, 'Descripcion del programa en cuestión'),
+(2, 'imagen2.jpg', 'titulo de desarrollo', 'articulo de desarrollo', 'develop', 2, 'Descripcion del artículo de desarrollo'),
+(3, 'imagen2.jpg', 'Desarrollo', 'Desarrollo', 'Fecha', 2, 'Usando tecnicas Front-End bla bla bla bla'),
+(4, 'imagen3.jpg', 'Cocina', 'Cocina', 'Cocina', 3, 'Hoy huevos fritos con patatas'),
+(5, 'imagen3.jpg', 'cocina', 'cocina', 'fecha', 3, 'Me paso al mundo vegano'),
+(6, 'imagen4.jpg', 'TEST', 'TEST', 'TEST', 5, 'TEST');
 
 -- --------------------------------------------------------
 
@@ -65,36 +70,8 @@ INSERT INTO `etiqueta` (`id_etiqueta`, `etiqueta`) VALUES
 (1, 'programas'),
 (2, 'desarrollo'),
 (3, 'cocina'),
-(4, 'obras');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `imagenes`
---
-
-CREATE TABLE `imagenes` (
-  `id_imagen` int(11) NOT NULL,
-  `imagen10` varchar(255) NOT NULL,
-  `imagen9` varchar(255) NOT NULL,
-  `imagen8` varchar(255) NOT NULL,
-  `imagen7` varchar(255) NOT NULL,
-  `imagen6` varchar(255) NOT NULL,
-  `imagen5` varchar(255) NOT NULL,
-  `imagen4` varchar(255) NOT NULL,
-  `imagen3` varchar(255) NOT NULL,
-  `imagen2` varchar(255) NOT NULL,
-  `imagen1` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `imagenes`
---
-
-INSERT INTO `imagenes` (`id_imagen`, `imagen10`, `imagen9`, `imagen8`, `imagen7`, `imagen6`, `imagen5`, `imagen4`, `imagen3`, `imagen2`, `imagen1`) VALUES
-(1, '', '', '', '', '', '', '', '', '', 'imagen1.png'),
-(2, '', '', '', '', '', '', '', '', '', 'imagen11.png'),
-(3, '', '', '', '', '', '', '', '', '', 'imagen21.png');
+(4, 'obras'),
+(5, 'test');
 
 -- --------------------------------------------------------
 
@@ -143,7 +120,6 @@ INSERT INTO `logotipo` (`id_logotipo`, `logotipo`) VALUES
 --
 ALTER TABLE `articulo`
   ADD PRIMARY KEY (`id_articulo`),
-  ADD KEY `imagenes_id` (`imagenes_id`,`etiqueta_id`),
   ADD KEY `etiqueta_id` (`etiqueta_id`);
 
 --
@@ -151,12 +127,6 @@ ALTER TABLE `articulo`
 --
 ALTER TABLE `etiqueta`
   ADD PRIMARY KEY (`id_etiqueta`);
-
---
--- Indices de la tabla `imagenes`
---
-ALTER TABLE `imagenes`
-  ADD PRIMARY KEY (`id_imagen`);
 
 --
 -- Indices de la tabla `indice`
@@ -178,19 +148,13 @@ ALTER TABLE `logotipo`
 -- AUTO_INCREMENT de la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `etiqueta`
 --
 ALTER TABLE `etiqueta`
-  MODIFY `id_etiqueta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `imagenes`
---
-ALTER TABLE `imagenes`
-  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_etiqueta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `indice`
@@ -212,8 +176,7 @@ ALTER TABLE `logotipo`
 -- Filtros para la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  ADD CONSTRAINT `articulo_ibfk_1` FOREIGN KEY (`etiqueta_id`) REFERENCES `etiqueta` (`id_etiqueta`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `articulo_ibfk_2` FOREIGN KEY (`imagenes_id`) REFERENCES `imagenes` (`id_imagen`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `articulo_ibfk_1` FOREIGN KEY (`etiqueta_id`) REFERENCES `etiqueta` (`id_etiqueta`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
