d573295bb83a1deabd1d5c36be68fb9d74e71a62
bolivar9721 <raul.ruiz.bolivar@gmail.com>
2020-10-29 20:34:14 +0100

    Poniendo el título del articulo

--- vista/v_articulo.php
+++ vista/v_articulo.php
@@ -1,7 +1,7 @@
-</div>
 <div class="card bg-dark text-white shadow-lg rounded col-12">
-<img src="vista/img/imagenes/<?php echo $articuloIndividual[0]['imagen']; ?>" class="card-img-top" alt="" style="max-height:17em;">
-  <div class="card-body">
-  <p class="card-text"><?php echo $articuloIndividual[0]['articulo']; ?></p>
-  </div>
+    <img src="vista/img/imagenes/<?php echo $articuloIndividual[0]['imagen']; ?>" class="card-img-top" alt="" style="max-height:17em;">
+    <h1 class="text-center"><?php echo $articuloIndividual[0]['titulo'] ?></h1>
+    <div class="card-body">
+        <p class="card-text"><?php echo $articuloIndividual[0]['articulo']; ?></p>
+    </div>
 </div>
